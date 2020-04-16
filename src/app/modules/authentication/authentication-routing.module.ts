ed91e2491621f31ed83e8bac543f56da1db795f7
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-04-16 13:44:16 -0300

    arrumando login e logout

--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -2,14 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { NgModule } from '@angular/core';
-import { LogoutComponent } from './logout/logout.component';
 
 
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent},
-    { path: 'registration', component: RegistrationComponent},
-    { path: 'logout', component: LogoutComponent}
+    { path: 'registration', component: RegistrationComponent}
   ];
 
 
